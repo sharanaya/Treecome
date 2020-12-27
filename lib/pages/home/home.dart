9dcdab5b4e5ab65929d0278d7f8fc1052210671b
Kartikey <ranjankartikey1999@gmail.com>
2020-12-28 05:27:40 +0530

    add navbar, logo

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,18 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:treecome/widgets/HomeView.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.icecream), onPressed: () {  },),
-        title: Text("Treecome"),
-        actions: [
-          FlatButton(onPressed: (){}, child: Text("Committee")),
-          FlatButton(onPressed: () {}, child: Text("Registration")),
-          FlatButton(onPressed: () {}, child: Text("Contact Us")),
-        ],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.account_circle_outlined),
+            onPressed: () {},
+          ),
+          title: Text('Treecome 2021'),
+          actions: [
+            SizedBox(
+              width: 800,
+              child: TabBar(tabs: [
+                Tab(
+                  child: Text(
+                    "Home",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "Committee",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "Registration",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "Contact us",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ]),
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            HomeView(),
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
       ),
-      body: Text("kdkdskldf"),
     );
   }
 }
